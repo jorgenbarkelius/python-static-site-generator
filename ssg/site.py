c6a6583d75785524743af10601fc5033ca6e98de
Jörgen Barkelius <jorgen.barkelius@delaval.com>
2023-03-05 21:19:17 +0100

    Finished the Site class and ssg.py

---
+++ ssg/site.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+
+
+class Site:
+    def __init__(self, source, dest):
+        self.source = source.Path()
+        self.dest = dest.Path()
+
+    def create_dir(self, path):
+        directory = self.dest / path.relative_to(self.source)
+        directory.mkdir(parents=True, exist_ok=True)
+
+    def build(self):
+        self.dest.mkdir(parents=True, exist_ok=True)
+        for path in self.source.rglob("*"):
+            if path == Site.directory:
+                Site.create_dir(path)
